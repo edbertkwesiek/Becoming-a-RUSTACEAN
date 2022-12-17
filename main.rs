cd9bca86852e740ddcf5ac18282d989629ffa389
edbert Gator (edgar) <36915781+edbertkwesiek@users.noreply.github.com>
2022-12-17 22:20:21 +0000

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,2 +1,26 @@
 
 
+use std::sync::{Mutex, Arc};
+use std::thread;
+
+fn main() {
+    let counter = Arc::new(Mutex::new(0));
+    let _hello = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+    for _ in 0..10{
+        let counter = Arc::clone(&counter);
+        let _hello = Arc::clone(&_hello);
+        let handle = thread::spawn( move || {
+            let mut num = counter.lock().unwrap();
+            
+
+            *num += 1;
+        });
+        handles.push(handle);
+        
+    }
+    for handle in handles{
+        handle.join().unwrap();
+    }
+    println!("Result : {}", *counter.lock().unwrap());
+}
